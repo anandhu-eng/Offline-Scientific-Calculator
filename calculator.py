2c2ddc0d47055097ee9812ecdc6419bfef550d9f
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2021-02-11 22:54:11 +0530

    updated the main calculator.py

--- calculator.py
+++ calculator.py
@@ -1,87 +1,139 @@
 from tkinter import *
-from PIL import Image,ImageTk
 
-window=Tk()
+window = Tk()
+window.title("Calculator")
 
+#iconphoto
+photo = PhotoImage(file="calacimg.png")
+window.iconphoto(False, photo)
 
+e = Entry(window, width=27, borderwidth=4)
+e.grid(row=0, column=0, columnspan=4, padx=0, pady=0, ipady=20)
 
+#button fuctions
 
-#entry box
-box=Entry(window,text=StringVar(),state=DISABLED)
-box.grid(row=0,column=0,columnspan=3)
+def button_click(number):
+    #e.delete(0, END)
+    current_val = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current_val) + str(number))
 
-#functions for each button
-def one():
-	one=1
-	print("clicked the button")
+def button_clear():
+    e.delete(0,END)
 
-def two():
-	two=2
-	print("clicked the button")
+def button_add():
+    fisrt_number = e.get()
+    global f_num
+    global math
+    math = "Addition"
+    f_num = float(fisrt_number)
+    e.delete(0, END)
 
-def three():
-	three=3
-	print("clicked the button")
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
 
-def four():
-	four=4
-	print("clicked the button")
-
-def five():
-	five=5
-	print("clicked the button")
-
-def six():
-	six=6
-	print("clicked the button")
-
-def seven():
-	seven=7
-	print("clicked the button")
-
-def eight():
-	eight=8
-	print("clicked the button")
-
-def nine():
-	nine=9
-	print("clicked the button")
-
-def zero():
-	zero=0
-	print("clicked the button")
+    if math == "Addition":
+        e.insert(0, f_num + float(second_number))
+    if math == "Multiplication":
+        e.insert(0, f_num * float(second_number))
+    if math == "Division":
+        e.insert(0, f_num / float(second_number))
+    if math == "Substraction":
+        e.insert(0, f_num - float(second_number))
 
 
 
-#buttons
-one=Button(window,text="1",command=one)
-one.grid(row=1,column=0)
+def button_substract():
+    fisrt_number = e.get()
+    global f_num
+    global math
+    math = "Substraction"
+    f_num = float(fisrt_number)
+    e.delete(0, END)
 
-two=Button(window,text="2",command=two)
-two.grid(row=1,column=1)
+def button_multiply():
+    fisrt_number = e.get()
+    global f_num
+    global math
+    math = "Multiplication"
+    f_num = float(fisrt_number)
+    e.delete(0, END)
 
-three=Button(window,text="3",command=three)
-three.grid(row=1,column=2)
+def button_divide():
+    fisrt_number = e.get()
+    global f_num
+    global math
+    math = "Division"
+    f_num = float(fisrt_number)
+    e.delete(0, END)
 
-four=Button(window,text="4",command=four)
-four.grid(row=2,column=0)
 
-five=Button(window,text="5",command=five)
-five.grid(row=2,column=1)
 
-six=Button(window,text="6",command=six)
-six.grid(row=2,column=2)
+# define buttons
 
-seven=Button(window,text="7",command=seven)
-seven.grid(row=3,column=0)
+button_1 = Button(window, text="1", padx=20, pady=10, command=lambda: button_click(1))
+button_2 = Button(window, text="2", padx=20, pady=10, command=lambda: button_click(2))
+button_3 = Button(window, text="3", padx=20, pady=10, command=lambda: button_click(3))
 
-eight=Button(window,text="8",command=eight)
-eight.grid(row=3,column=1)
+button_4 = Button(window, text="4", padx=20, pady=10, command=lambda: button_click(4))
+button_5 = Button(window, text="5", padx=20, pady=10, command=lambda: button_click(5))
+button_6 = Button(window, text="6", padx=20, pady=10, command=lambda: button_click(6))
 
-nine=Button(window,text="9",command=nine)
-nine.grid(row=3,column=2)
+button_7 = Button(window, text="7", padx=20, pady=10, command=lambda: button_click(7))
+button_8 = Button(window, text="8", padx=20, pady=10, command=lambda: button_click(8))
+button_9 = Button(window, text="9", padx=20, pady=10, command=lambda: button_click(9))
 
-zero=Button(window,text="0",command=zero)
-zero.grid(row=4,column=1)
+button_0 = Button(window, text="0", padx=19, pady=10, command=lambda: button_click(0))
+button_dot = Button(window, text=".", padx=21, pady=10, command=lambda: button_click('.'))
+button_equal = Button(window, text="=", padx=19, pady=10, command=button_equal)
+
+button_clear = Button(window, text="CLR", padx=12, pady=10, command=button_clear)
+button_divide = Button(window, text="/", padx=20, pady=10, command=button_divide)
+
+button_multiply = Button(window, text="X", padx=19, pady=10, command=button_multiply)
+
+button_substract = Button(window, text="-", padx=20, pady=10, command=button_substract)
+button_add = Button(window, text="+", padx=18, pady=10, command=button_add)
+
+button_tan=Button(window, text="tan", padx=14, pady=10)
+button_cos=Button(window, text="cos", padx=14, pady=10)
+button_sin=Button(window, text="sin", padx=14, pady=10)
+
+
+
+
+#put buttons on the display
+
+button_clear.grid(row=5, column=2)
+button_divide.grid(row=1, column=3)
+
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
+button_add.grid(row=4, column=3)
+
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
+button_substract.grid(row=3, column=3)
+
+
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
+button_multiply.grid(row=2, column=3)
+
+button_0.grid(row=5, column=1)
+button_equal.grid(row=5, column=3)
+button_dot.grid(row=5, column=0)
+
+button_tan.grid(row=1,column=0)
+button_sin.grid(row=1,column=1)
+button_cos.grid(row=1,column=2)
+
+
+
+
 
 window.mainloop()
